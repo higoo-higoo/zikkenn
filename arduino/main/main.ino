527372b8699df9f8b92d5e5e18842f1527c44178
higoo-higoo <higyuki1101@gmail.com>
2024-04-26 14:07:17 +0900

    serial.ino

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -10,26 +10,6 @@ int nose = 0;
 int leftmouth = 0;
 int rightmouth = 0;
 
-void serialSetup() {
-	Serial.begin(9600);
-}
-
-void serialLoop() {
-	if (Serial.available() > 0) {
-		char letter = Serial.read();
-		if (letter == 'b') { // eyeblow
-      eyeblow = Serial.parseInt();
-		} else if (letter == 'e') { // eye
-      eye = Serial.parseInt();
-    } else if (letter == 'n') { // nose
-      nose = Serial.parseInt();
-    } else if (letter == 'l') { // left mouth
-      leftmouth = Serial.parseInt();
-    } else if (letter == 'r') { // right mouth
-      rightmouth = Serial.parseInt();
-    }
-	}
-}
 void setup() {
   serialSetup();
   pinMode(EYEBLOWPORT, OUTPUT);

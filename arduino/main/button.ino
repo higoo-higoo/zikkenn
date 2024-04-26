61db5d5a4c70a982dc8398fb721bf22dd84c61bf
higoo-higoo <higyuki1101@gmail.com>
2024-04-26 16:02:34 +0900

    mono

--- arduino/main/button.ino
+++ arduino/main/button.ino
@@ -7,10 +7,8 @@ const int rightmouthButton = 6;
 const int INPUTPORTS[5] = {eyeblowButton, eyeButton, noseButton, leftmouthButton, rightmouthButton};
 
 // ONOFF
-int prev_input[5] = {0, 0, 0, 0, 0};
+int input_count[5] = {0, 0, 0, 0, 0};
 
-//
-int prev_0_count[5] = {0, 0, 0, 0, 0};
 
 // time_from last change
 float time_last_change[5] = {0, 0, 0, 0, 0};
@@ -28,47 +26,13 @@ void buttonLoop()
   for (int i = 0; i < 5; i++)
   {
     int input = digitalRead(INPUTPORTS[i]);
-
-    if (i == 0) {
-      Serial.print("Eyeblow: ");
-      Serial.println(input);
-    }
-    if (input){
-      prev_0_count[i] = 0;
-    if (!prev_input[i]){
-      time_last_change[i] = millis();
-      prev_input[i] = input;
-      // Serial.print("Button ");
-      // Serial.print(i);
-      // Serial.print(" is ");
-      // Serial.println(input);
-    }
-    } else if (prev_input[i]) {
-      prev_0_count[i]++;
-      if (prev_0_count[i] > 5)
-      {
-        prev_input[i] = 0;
-        time_last_change[i] = millis();
-        prev_0_count[i] = 0;
-      }
-    }
-  }
-}
-
-void outputUpdate() {
-  for (int i = 0; i < 5; i++)
-  {
-    if (millis() - time_last_change[i] > 1000 / 16)
-    {
-      output[i] = 0;
+    if (input) {
+      input_count[i]++;
     } else {
-      output[i] = 1;
+      input_count[i] = 0;
     }
-    if (i == 0)
-    {
-      // Serial.print("Eyeblow: ");
-      // Serial.println(output[i]);
+    if (input_count[i] > 5) {
+      output[i] = 0;
     }
   }
 }
-
--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -7,7 +7,7 @@ const int RIGHTMOUTHPORT = 12;
 const int OUTPUTPORTS[5] = {EYEBLOWPORT, EYEPORT, NOSEPORT, LEFTMOUTHPORT, RIGHTMOUTHPORT};
 
 
-int output[5] = {0, 0, 0, 0, 0};
+int output[5] = {1, 1, 1, 1, 1};
 
 void setup() {
   serialSetup();
@@ -21,7 +21,6 @@ void setup() {
 void loop() {
   // serialLoop();
   buttonLoop();
-  outputUpdate();
   for (int i = 0; i < 5; i++)
   {
     digitalWrite(OUTPUTPORTS[i], output[i]);

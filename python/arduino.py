2da208b6d4b4f07a793e07cf66ee5780db7e5e07
higoo-higoo <higyuki1101@gmail.com>
2024-05-10 19:47:52 +0900

    add keyboard control

--- python/arduino.py
+++ python/arduino.py
@@ -1,4 +1,5 @@
 import serial
+import time
 import argparse
 
 class Arduino:
@@ -9,9 +10,9 @@ class Arduino:
 
     def write(self, part_name, value):
         if value not in[0, 1]:
-            raise ValueError("Value must be 0 or 1")
+            print("Value must be 0 or 1")
         if part_name not in ["b", "e", "n", "l", "r"]:
-            raise ValueError("Part name must be led or buzzer")
+            print("Part name must be led or buzzer")
         self.serial.write(f"{part_name}{value}\n".encode())
 
     def __del__(self):
@@ -22,7 +23,25 @@ if __name__ == "__main__":
     arg_parser.add_argument("--port", type=str, required=True)
     args = arg_parser.parse_args()
     arduino = Arduino(args.port)
-    while True:
-        part_name = input("Enter part name: ")
-        value = int(input("Enter value: "))
-        arduino.write(part_name, value)
+    input("start moving")
+    time.sleep(1)
+    arduino.write("b", 1)
+    time.sleep(1)
+    arduino.write("e", 1)
+    time.sleep(1)
+    arduino.write("n", 1)
+    time.sleep(1)
+    arduino.write("l", 1)
+    time.sleep(1)
+    input("press enter to stop eye blow")
+    arduino.write("b", 0)
+    input("press enter to stop eye")
+    arduino.write("e", 0)
+    input("press enter to stop nose")
+    arduino.write("n", 0)
+    input("press enter to stop mouth")
+    arduino.write("l", 0)
+    # while True:
+    #     part_name = input("Enter part name: ")
+    #     value = int(input("Enter value: "))
+    #     arduino.write(part_name, value)
